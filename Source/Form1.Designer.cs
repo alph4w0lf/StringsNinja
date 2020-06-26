8f001ffa13df257d4ef4be90ffe0393fabd946be
Hussien Yousef <18534942+alph4w0lf@users.noreply.github.com>
2020-06-27 00:51:42 +0300

    Add files via upload

---
+++ Source/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace StringsNinja
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(39, 27);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(551, 294);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(351, 377);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(239, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Encode";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(39, 377);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(239, 37);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Decode";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Input/Output:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(203, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(192, 33);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Processing ...";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 341);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Algorithm:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Reverse String",
+            "Base64",
+            "Ninja Substitution",
+            "Combination of All"});
+            this.comboBox1.Location = new System.Drawing.Point(95, 338);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(495, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 423);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Strings Ninja 1.0    by Hussien Yousef (alph4w0lf)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Source/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace StringsNinja
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // Algorithm Functions
+        private string Reverse_String(string input)
+        {
+            string output;
+            char[] chr_array = input.ToCharArray();
+            Array.Reverse(chr_array);
+            output = new string(chr_array);
+            return output;
+        }
+
+        private string Base64_Encode(string input)
+        {
+            string output;
+            output = System.Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(input));
+            return output;
+        }
+
+        private string Base64_Decode(string input)
+        {
+            try
+            {
+                string output;
+                output = System.Text.Encoding.UTF8.GetString(System.Convert.FromBase64String(input));
+                return output;
+            }
+            catch
+            {
+                MessageBox.Show("Your input string is not Base64 Encoded.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return input;
+            }         
+        }
+
+        private Dictionary<string, string> Ninja_Map()
+        {
+            Dictionary<string, string> ninja_mapping = new Dictionary<string, string>();
+            ninja_mapping.Add("a", "[[1]]");
+            ninja_mapping.Add("b", "[[2]]");
+            ninja_mapping.Add("c", "[[3]]");
+            ninja_mapping.Add("d", "[[4]]");
+            ninja_mapping.Add("e", "[[5]]");
+            ninja_mapping.Add("f", "[[6]]");
+            ninja_mapping.Add("z", "[[7]]");
+            ninja_mapping.Add("x", "[[8]]");
+            ninja_mapping.Add("y", "[[9]]");
+            ninja_mapping.Add("G", "[[10]]");
+            ninja_mapping.Add("A", "[[11]]");
+            ninja_mapping.Add("B", "[[12]]");
+            ninja_mapping.Add("m", "[[13]]");
+            ninja_mapping.Add("l", "[[14]]");
+            ninja_mapping.Add("v", "[[15]]");
+            ninja_mapping.Add("k", "[[16]]");
+            return ninja_mapping;
+        }
+
+        private string Ninja_Encode(string input)
+        {
+            string output = input;
+            // Substitution Map
+            Dictionary<string, string> ninja_map = Ninja_Map();
+            foreach (KeyValuePair<string, string> map_pair in ninja_map)
+            {
+                output = output.Replace(map_pair.Key, map_pair.Value);
+            }
+            return output;
+        }
+
+        private string Ninja_Decode(string input)
+        {
+            string output = input;
+            // Substitution Map
+            Dictionary<string, string> ninja_map = Ninja_Map();
+            foreach (KeyValuePair<string, string> map_pair in ninja_map)
+            {
+                output = output.Replace(map_pair.Value, map_pair.Key);
+            }
+            return output;
+        }
+
+        private void Lock_User()
+        {
+            label2.Visible = true;
+            textBox1.Enabled = false;
+            button1.Enabled = false;
+            button2.Enabled = false;
+            comboBox1.Enabled = false;
+        }
+
+        private void Unlock_User()
+        {
+            label2.Visible = false;
+            textBox1.Enabled = true;
+            button1.Enabled = true;
+            button2.Enabled = true;
+            comboBox1.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Check if algorithm is chosen
+            if(comboBox1.SelectedIndex < 0)
+            {
+                MessageBox.Show("You have to choose an Encoding algorithm first!");
+                return;
+            }
+
+            // Lock User
+            Lock_User();
+
+
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    // Perform Reverse String Encoding
+                    textBox1.Text = Reverse_String(textBox1.Text);
+                    break;
+                case 1:
+                    // Perform base64 Encoding
+                    textBox1.Text = Base64_Encode(textBox1.Text);
+                    break;
+                case 2:
+                    // Perform Ninja Substitution Encoding
+                    textBox1.Text = Ninja_Encode(textBox1.Text);
+                    break;
+                case 3:
+                    // Perform Combination of All Encoding
+                    textBox1.Text = Ninja_Encode(Base64_Encode(Reverse_String(textBox1.Text)));
+                    break;
+            }
+
+            // Unlock User
+            Unlock_User();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Check if algorithm is chosen
+            if (comboBox1.SelectedIndex < 0)
+            {
+                MessageBox.Show("You have to choose a Decoding algorithm first!");
+                return;
+            }
+
+            // Lock User
+            Lock_User();
+
+
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    // Perform Reverse String Decoding
+                    textBox1.Text = Reverse_String(textBox1.Text);
+                    break;
+                case 1:
+                    // Perform base64 Decoding
+                    textBox1.Text = Base64_Decode(textBox1.Text);
+                    break;
+                case 2:
+                    // Perform Ninja Substitution Decoding
+                    textBox1.Text = Ninja_Decode(textBox1.Text);
+                    break;
+                case 3:
+                    // Perform Combination of All Decoding
+                    textBox1.Text = Reverse_String(Base64_Decode(Ninja_Decode(textBox1.Text)));
+                    break;
+            }
+
+            // Unlock User
+            Unlock_User();
+        }
+    }
+}
